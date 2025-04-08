72c213655a0fc5fc792692fbfa7d6c6c5ef22dfd
m0rgan7 <lavinia.p.a.melo@gmail.com>
2025-04-08 12:28:47 -0300

    new file ExemploFor.java

---
+++ EstruturasCondicionais/ExemploFor.java
@@ -0,0 +1,12 @@
+public class ExemploFor{
+    public static void main (String[] args){
+
+    //for (declaraçao da variavel; validaçao; e a incrementação){}
+
+    for (int carneirinhos = 2; carneirinhos <=20; carneirinhos ++){
+
+        System.out.println ("Contando carneirinhos  " + carneirinhos);
+       
+    }
+    }
+}
