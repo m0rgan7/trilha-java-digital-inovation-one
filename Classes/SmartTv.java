169b17ec4f5da1761aac47adfdebf9f23b36dd83
m0rgan7 <lavinia.p.a.melo@gmail.com>
2025-04-09 17:21:29 -0300

    modified:   Classes/SmartTv.java modified:   Classes/Usuario.java

--- Classes/SmartTv.java
+++ Classes/SmartTv.java
@@ -2,9 +2,8 @@ public class SmartTv {
     
     boolean ligada=false;
     int canal =1;
-    int volume = 50;
+    int volume = 60;
     
-
     public void aumentarVolume(){
       volume++;
     }
--- Classes/Usuario.java
+++ Classes/Usuario.java
@@ -1,9 +1,6 @@
-//o package nao funciona pq tem "-" mas ta salvo dessa forma na minha pasta, além disso tem algum erro que não vejo
-package Dio.edu.morgan.classesjava.dio-trilha-java-basico;
-
 public class Usuario {
     
-    public static void main(String[] args) throws Exeption {
+    public static void main(String[] args) throws Exception {
         
         SmartTv smartTv = new SmartTv();
 
@@ -13,15 +10,15 @@ public class Usuario {
         smartTv.diminuirVolume();
         System.out.println("Volume atual " + smartTv.volume);
 
-        System.out.println("tv ligada?" + smartTv.ligada);
-        System.out.println("qual o canal atual?" + smartTv.canal);
-        System.out.println("qual o volume atual?" + smartTv.volume);
+        System.out.println("tv ligada? " + smartTv.ligada);
+        System.out.println("qual o canal atual? " + smartTv.canal);
+        System.out.println("qual o volume atual? " + smartTv.volume);
 
         smartTv.ligar();
         System.out.println("Novo status -> TV Ligada? " + smartTv.ligada);
 
-        smartTV.desligar();
-        System.out.println("Novo status -> TV desligada?" + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("Novo status -> TV desligada? " + smartTv.ligada);
 
     }
 } 
