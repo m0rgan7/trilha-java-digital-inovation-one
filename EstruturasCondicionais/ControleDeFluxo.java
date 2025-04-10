d46026308313b43a7edbc31212c8a4a2efdbca17
m0rgan7 <lavinia.p.a.melo@gmail.com>
2025-04-09 21:14:03 -0300

    modified:   EstruturasCondicionais/ControleDeFluxo.java

--- EstruturasCondicionais/ControleDeFluxo.java
+++ EstruturasCondicionais/ControleDeFluxo.java
@@ -33,3 +33,44 @@ class ParametrosInvalidosException extends Exception{
     public ParametrosInvalidosException(String message) {
     }
 }
+
+/*Esse não está muito profissional, faz outro seguindo:
+
+import java.util.Scanner;
+
+ public class ControleDeFluxo {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+	
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+            terminal.close();
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		
+		
+		int contagem = parametroDois - parametroUm;
+
+		for (int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
+	}
+}
+
+class ParametrosInvalidosException extends Exception{
+   public ParametrosInvalidosException(String message){
+     super(message);
+   }
+}
+
+ */
