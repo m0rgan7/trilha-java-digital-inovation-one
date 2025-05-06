1344977dcf4665d45555f24fa91e12ea2246cbb5
m0rgan7 <lavinia.p.a.melo@gmail.com>
2025-05-06 15:33:50 -0300

    modified:   Desenvolvimento-de-API's/my-first-web-api/src/main/java/dio/my_first_web_api/controller/UsuarioController.java

--- Desenvolvimento-de-API's/my-first-web-api/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
+++ Desenvolvimento-de-API's/my-first-web-api/src/main/java/dio/my_first_web_api/controller/UsuarioController.java
@@ -3,15 +3,30 @@ package dio.my_first_web_api.controller;
 import java.util.List;
 import dio.my_first_web_api.model.Usuario;
 import dio.my_first_web_api.repository.UsuarioRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsuarioController {
+
     @Autowired
     private UsuarioRepository repository;
+
+    @GetMapping ("/users")
     public List<Usuario> getUsuarios(){
         return repository.findALL();
+    }
+
+    @GetMapping ("/users/{username)")
+    public Usuario geOne(@PathVariable("username") String username){
+	return repository.findByUserNnme(username);
+    }
+
+    @DeleteMapping ("/users/{id}")
+    public void deleteUser(@PathVariable("id") Integer id){
+        repository.deleteById(id);
     }
 }
