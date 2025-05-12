20790b67e3079006ce0b4b0fa72c52d63485d665
Lavínia Melo <161097407+m0rgan7@users.noreply.github.com>
2025-05-12 18:40:46 -0300

    Create MediasNotas.java
    Depois mova ela para a pasta correta pois estou fazendo no pc das faculdade no momento e nao tem como usar o git.

---
+++ programação-estruturada/MediasNotas.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MediaNotas{
+    public static void main (String [] args){
+        Scanner input = new Scanner;
+
+        do {
+            System.out.println ("Digite a notas que deseja calcular a média: ");
+            float nota = input.nextFloat();
+
+             System.out.println("Deseja parar? ");
+             String continuar = input.nextString;
+
+            if (continuar = "nao"){
+              break;
+
+        } while (continuar = "sim");
+  //esqueci de calcular a media e acho que a estrutura ta toda errada, nao tenho ideia de como que criam novas variaveis "notas" automaticamente
+        scanner.close();
+        }
+  }
+}
